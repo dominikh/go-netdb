23a50d4f44e64d4eb8a54a6a0a2cf09f7b9d9d01
Dominik Honnef <dominikh@fork-bomb.org>
2013-01-11 17:13:29 +0100

    return pointers and expect pointers instead of strings

--- netdb.go
+++ netdb.go
@@ -3,6 +3,10 @@
 //
 // A pure Go implementation is used by parsing /etc/protocols and
 // /etc/services
+//
+// All return values are pointers that point to the entries in the
+// original list of protocols and services. Manipulating the entries
+// would affect the entire program.
 package netdb
 
 import (
@@ -21,18 +25,19 @@ type Servent struct {
 	Name     string
 	Aliases  []string
 	Port     int
-	Protocol string
+	Protocol *Protoent
 }
-
 
 // These variables get populated from /etc/protocols and /etc/services
 // respectively.
 var (
-	Protocols []Protoent
-	Services []Servent
+	Protocols []*Protoent
+	Services  []*Servent
 )
 
 func init() {
+	protoMap := make(map[string]*Protoent)
+
 	// Load protocols
 	data, err := ioutil.ReadFile("/etc/protocols")
 	if err != nil {
@@ -52,11 +57,14 @@ func init() {
 			panic(err)
 		}
 
-		Protocols = append(Protocols, Protoent{
+		protoent := &Protoent{
 			Name:    fields[0],
 			Aliases: fields[2:],
 			Number:  int(num),
-		})
+		}
+		Protocols = append(Protocols, protoent)
+
+		protoMap[fields[0]] = protoent
 	}
 
 	// Load services
@@ -83,82 +91,89 @@ func init() {
 		proto := portproto[1]
 		aliases := fields[2:]
 
-		Services = append(Services, Servent{
+		Services = append(Services, &Servent{
 			Name:     name,
 			Aliases:  aliases,
 			Port:     int(port),
-			Protocol: proto,
+			Protocol: protoMap[proto],
 		})
 	}
 }
 
 // Equal checks if two Protoents are the same, which is the case if
-// their protocol numbers are identical.
-func (this Protoent) Equal(other Protoent) bool {
+// their protocol numbers are identical or both are nil.
+func (this *Protoent) Equal(other *Protoent) bool {
+	if this == nil && other == nil {
+		return true
+	}
+
+	if this == nil || other == nil {
+		return false
+	}
+
 	return this.Number == other.Number
 }
 
-// GetProtoByNumber returns the Protoent for the correspondent
-// protocol number.
-func GetProtoByNumber(num int) (protoent Protoent, ok bool) {
+// GetProtoByNumber returns the Protoent for a given protocol number.
+func GetProtoByNumber(num int) (protoent *Protoent) {
 	for _, protoent := range Protocols {
 		if protoent.Number == num {
-			return protoent, true
+			return protoent
 		}
 	}
-	return Protoent{}, false
+	return nil
 }
 
 // GetProtoByName returns the Protoent whose name or any of its
 // aliases matches the argument.
-func GetProtoByName(name string) (protoent Protoent, ok bool) {
+func GetProtoByName(name string) (protoent *Protoent) {
 	for _, protoent := range Protocols {
 		if protoent.Name == name {
-			return protoent, true
+			return protoent
 		}
 
 		for _, alias := range protoent.Aliases {
 			if alias == name {
-				return protoent, true
+				return protoent
 			}
 		}
 	}
 
-	return Protoent{}, false
+	return nil
 }
 
-// GetServByName returns the Servent for a given service name and
-// protocol name. If the protocol name is empty, the first service
-// matching the service name is returned.
-func GetServByName(name, protocol string) (servent Servent, ok bool) {
+// GetServByName returns the Servent for a given service name or alias
+// and protocol. If the protocol is nil, the first service matching
+// the service name is returned.
+func GetServByName(name string, protocol *Protoent) (servent *Servent) {
 	for _, servent := range Services {
-		if servent.Protocol != protocol && protocol != "" {
+		if !servent.Protocol.Equal(protocol) {
 			continue
 		}
 
 		if servent.Name == name {
-			return servent, true
+			return servent
 		}
 
 		for _, alias := range servent.Aliases {
 			if alias == name {
-				return servent, true
+				return servent
 			}
 		}
 	}
 
-	return Servent{}, false
+	return nil
 }
 
 // GetServByPort returns the Servent for a given port number and
-// protocol name. If the protocol name is empty, the first service
-// matching the port number is returned.
-func GetServByPort(port int, protocol string) (Servent, bool) {
+// protocol. If the protocol is nil, the first service matching the
+// port number is returned.
+func GetServByPort(port int, protocol *Protoent) *Servent {
 	for _, servent := range Services {
-		if servent.Port == port && (servent.Protocol == protocol || protocol == "") {
-			return servent, true
+		if servent.Port == port && servent.Protocol.Equal(protocol) {
+			return servent
 		}
 	}
 
-	return Servent{}, false
+	return nil
 }
